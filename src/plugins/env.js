8a831ef81ca3b4824587786b23dd8f60b48f1a85
Ronald <ronald-farias@outlook.com>
2018-08-17 11:24:17 -0300

    removed .env plugin

--- src/plugins/env.js
+++
@@ -1,9 +0,0 @@
-// import something here
-import dotenv from 'dotenv';
-
-// leave the export, even if you don't use it
-export default ({ app, router, Vue }) => {
-  // something to do
-  dotenv.load();
-  process.env.WATCH_LINK='http://0baafd03.ngrok.io'
-}
