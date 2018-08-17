3076c957aae14b0524d89e977507550f93c684b1
Ronald <ronald-farias@outlook.com>
2018-08-17 09:55:34 -0300

    added QChip to Quasar

--- quasar.conf.js
+++ quasar.conf.js
@@ -35,6 +35,7 @@ module.exports = function (ctx) {
     // framework: 'all' --- includes everything; for dev only!
     framework: {
       components: [
+        'QChip',
         'QTabs',
         'QTab',
         'QTabPane',
