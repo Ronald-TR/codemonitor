927237f524b271a8e8137c3cedb29481abf9424a
Ronald <ronald-farias@outlook.com>
2018-08-21 11:42:07 -0300

    created default page for user infos

--- src/pages/user.vue
+++ src/pages/user.vue
@@ -1,14 +1,42 @@
 <template>
   <q-page padding>
-    <!-- content -->
+    <div class="container">
+      <p class="q-title">Dados do Usuário</p>
+      <ul class="">
+        <li><strong>Nome de exibição:</strong> {{ this.userinfo.display_name }}</li>
+        <li><strong>Usuario: </strong>{{ this.userinfo.username }}</li>
+        <li><strong>Data de Cadastro: </strong> {{ this.userinfo.join_date }} </li>
+      </ul>
+    </div>
   </q-page>
 </template>
 
 <script>
 export default {
-  // name: 'PageName',
+  name: 'user',
+  data () {
+    return {
+      userinfo: null
+    }
+  },
+  mounted () {
+    this.userinfo = this.$store.state.auth.userinfo
+  }
 }
 </script>
 
 <style>
+.container {
+  /* display: flex; */
+  background-color: white;
+  border-radius: 5px;
+  padding: 20px;
+}
+
+ul {
+  /* list-style: none; */
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
 </style>
