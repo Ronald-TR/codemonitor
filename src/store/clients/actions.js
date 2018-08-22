c438a8f1d6a18671609facdda246294243fb4d72
Ronald <ronald-farias@outlook.com>
2018-08-22 09:14:57 -0300

    added UPDATE and DELETE actions for client

--- src/store/clients/actions.js
+++ src/store/clients/actions.js
@@ -1,11 +1,45 @@
 import Axios from 'axios'
+import store from '../../store'
 /*
 export function someAction (context) {
 }
 */
-const base_url = 'http://'
+const base_url = 'http://apicodemonitor.herokuapp.com/'
 export function GET_ALL_SERVICES ({commit, dispatch}, user) {
     return new Promise((resolve, reject) => {
         Axios.get(base_url + 'all')
+    })
+}
+
+export function UPDATE_CLIENT( {commit, dispatch}, userclient ) {
+    return new Promise( (resolve, reject ) => {
+        const data = {
+            'id': userclient.id,
+            'name': userclient.name,
+            'watchlink': userclient.watchlink 
+        }
+        const headers = {
+            'Authorization': store().state.auth.token,
+            'Content-Type': 'application/json'
+        }
+        
+        Axios({url: base_url + 'clients', data: data, method: 'PATCH', headers: headers }).then(resp => {
+            resolve(resp)    
+        }).catch(err => {
+            reject(err)
+        })
+    })
+}
+
+export function DELETE_CLIENT( {commit, dispatch}, userclient ) {
+    return new Promise( (resolve, reject ) => {
+        const data = {
+            'id': userclient.id,
+        }
+        Axios({url: base_url + 'clients', data: data, method: 'DELETE' }).then(resp => {
+            resolve(resp)    
+        }).catch(err => {
+            reject(err)
+        })
     })
 }
